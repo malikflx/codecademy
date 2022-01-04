b2e3b2646981ee298715349806bbb85550b7b6ff
Malik Felix <malikfelix50@gmail.com>
2022-01-03 20:22:31 -0500

    add number guesser functionality in

--- script.js
+++ script.js
@@ -3,4 +3,29 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+}
 
+const compareGuesses = (human, computer, target) => {
+  if (Math.abs(human - target) <= Math.abs(computer - target)) {
+    return true
+  } else {
+    return false;
+  }
+}
+
+const updateScore = score => {
+  if (score === 'human') {
+    humanScore++;
+  } else {
+    computerScore++;
+  }
+}
+
+const advanceRound = () => {
+  currentRoundNumber++;
+}
+
+// console.log(generateTarget());
+// console.log(compareGuesses(2, 4, 3));
